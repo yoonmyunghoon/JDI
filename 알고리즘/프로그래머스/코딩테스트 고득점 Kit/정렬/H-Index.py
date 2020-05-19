5166900db89aefff5766938b09233a0b43a90a58
yoonmyunghoon <youn1791472@gmail.com>
2020-05-20 00:46:25 +0900

    1sol

---
+++ 알고리즘/프로그래머스/코딩테스트 고득점 Kit/정렬/H-Index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    h = 1
+    if len(citations) == 1:
+        return 1
+    if citations[0] == citations[-1]:
+        return min(len(citations), citations[0])
+    while h <= len(citations) and h <= citations[h-1]:
+        h += 1
+    return h-1
+
+citations = [3, 0, 6, 1, 5]
+
+print(solution(citations))
