86c72eb6b472eb25c5277301dae0b3155c42363c
yoonmyunghoon <youn1791472@gmail.com>
2020-08-04 15:02:44 +0900

    edit bj12100

--- 알고리즘/백준/12100_2046(Easy).py
+++ 알고리즘/백준/12100_2046(Easy).py
@@ -6,12 +6,14 @@ arr = [0, 1, 2, 3]
 q = 5
 T = [0]*q
 
+
+# 중복순열
 def perm(n, r):
     global maximum
     if r == 0:
-        MAX = move(T)
-        if MAX > maximum:
-            maximum = MAX
+        max_in_blocks = move(T)
+        if max_in_blocks > maximum:
+            maximum = max_in_blocks
         return
     else:
         for i in range(n-1, -1, -1):
@@ -20,11 +22,14 @@ def perm(n, r):
             perm(n, r-1)
             arr[i], arr[n-1] = arr[n-1], arr[i]
 
+
+# block 이동
 def move(T):
     blocks = copy.deepcopy(blocks_og)
     for i in range(len(T)):
         if T[i] == 0:
             for y in range(N):
+                # 밀기
                 cnt = 0
                 stores = []
                 for k in range(N):
@@ -36,7 +41,7 @@ def move(T):
                         blocks[l][y] = stores[l]
                     else:
                         blocks[l][y] = 0
-
+                # 합치기
                 x = 1
                 while x < N:
                     if blocks[x][y] == blocks[x-1][y]:
@@ -44,7 +49,7 @@ def move(T):
                         blocks[x][y] = 0
                         x += 1
                     x += 1
-
+                # 밀기
                 cnt = 0
                 stores = []
                 for k in range(N):
@@ -155,12 +160,13 @@ def move(T):
                         blocks[x][l] = stores[l]
                     else:
                         blocks[x][l] = 0
-    MAX = 0
+
+    max_in_blocks = 0
     for i in range(N):
         for j in range(N):
-            if blocks[i][j] > MAX:
-                MAX = blocks[i][j]
-    return MAX
+            if blocks[i][j] > max_in_blocks:
+                max_in_blocks = blocks[i][j]
+    return max_in_blocks
 
 
 N = int(input())
