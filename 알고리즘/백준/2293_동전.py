c456f9b28c681f6bdae095eb7a37796902b02497
yoonmyunghoon <youn1791472@gmail.com>
2020-09-24 16:22:25 +0900

    동전1

--- 알고리즘/백준/2293_동전.py
+++ 알고리즘/백준/2293_동전.py
@@ -1,3 +1,12 @@
 import sys
 sys.stdin = open("2293_동전.txt")
 
+n, k = map(int, input().split())
+coins = [int(input()) for i in range(n)]
+D = [0]*(k+1)
+D[0] = 1
+for x in range(n):
+    for y in range(k+1):
+        if coins[x] <= y:
+            D[y] += D[y-coins[x]]
+print(D[k])
