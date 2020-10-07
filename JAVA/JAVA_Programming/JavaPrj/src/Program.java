f20ab67d6d8b2135f2f06dc7bac92d243af38d09
yoonmyunghoon <youn1791472@gmail.com>
2020-10-07 21:36:00 +0900

    docs: Add JAVA_Programming in JAVA

---
+++ JAVA/JAVA_Programming/JavaPrj/src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+	public static void main(String[] args) {
+
+		int a = 50;
+		int b = 40;
+		int c = 30;
+		int total = a + b + c;
+
+		System.out.printf("total is %d\n", total);
+	}
+}
