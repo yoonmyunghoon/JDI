ee942f4cdfb7e2bf6c9d4f94b87c5158ef83c3f4
yoonmyunghoon <youn1791472@gmail.com>
2021-03-13 20:02:27 +0900

    Solve 3079_입국심사

--- 알고리즘/백준/3079_입국심사.py
+++ 알고리즘/백준/3079_입국심사.py
@@ -3,24 +3,23 @@ sys.stdin = open('3079_입국심사.txt')
 # input = sys.stdin.readline
 
 
-def find_time(start, end):
-    time = 0
-    list_len = len(judge_times)
+def find_min_time(start, end):
     while start < end:
+        passed_person_cnt = 0
         mid = (start + end) // 2
-        min_value = 10**9
-        min_index = 0
-        for i in range(list_len):
-            if judge_times[i] < min_value:
-                min_index = i
-        time = judge_times[i]
-        judge_times[i] += judge_times[i]
-
+        for t in judge_times:
+            passed_person_cnt += mid//t
+        if passed_person_cnt < M:
+            start = mid + 1
+        else:
+            end = mid
+    return end
 
 
 N, M = map(int, input().split())
 judge_times = [int(input()) for _ in range(N)]
-print(N, M, judge_times)
-find_time(1, (M//N+1)*max(judge_times))
+len_of_list = len(judge_times)
+max_of_time = max(judge_times)
+print(find_min_time(1, (M//N+1)*max_of_time))
 
 
