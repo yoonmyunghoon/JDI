55ed72da98c7de3509aa6e52847026d7728bb6cc
yoonmyunghoon <youn1791472@gmail.com>
2021-03-15 21:52:03 +0900

    Edit 16434_드래곤 앤 던전

--- 알고리즘/백준/16434_드래곤 앤 던전.py
+++ 알고리즘/백준/16434_드래곤 앤 던전.py
@@ -1,19 +1,31 @@
-import sys
+import sys, math
 sys.stdin = open('16434_드래곤 앤 던전.txt')
 
 
-# def find_HP(start, end):
-#     while start < end:
-#         mid = (start+end)//2
-#         for i in range(N):
-#             if room[0] == 1: # 몬스터
-#
-#             else: # 포션
-
+def find_HP(start, end):
+    while start < end:
+        mid = (start+end)//2
+        hp = mid
+        atk = A
+        flag = 0
+        for room in rooms:
+            if room[0] == 1: # 몬스터
+                if math.ceil(room[2]//atk) >= math.ceil(hp//room[1]):
+                    hp -= math.ceil(hp//room[1])
+                else:
+                    flag = -1
+                    break
+            else: # 포션
+                atk += room[1]
+                hp += min(mid, hp + room[2])
+        print(start, mid, end)
+        if flag == -1:
+            start = mid + 1
+        else:
+            end = mid
+    return end
 
 
 N, A = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
-print(N, A)
-print(room)
-# print(find_HP(1, 123456*1000000))
+rooms = [list(map(int, input().split())) for _ in range(N)]
+print(find_HP(1, 123456*1000000))
