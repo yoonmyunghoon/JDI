139e750370681eeb701c159155bf216fdeb989b9
yoonmyunghoon <youn1791472@gmail.com>
2021-07-29 21:21:03 +0900

    Edit desc bj16681

--- 알고리즘/백준/16681_등산.py
+++ 알고리즘/백준/16681_등산.py
@@ -1,7 +1,12 @@
 import sys
 sys.stdin = open("16681_등산.txt")
 # input = sys.stdin.readline
-
+'''
+집에서 갈 수 있는 경로들, 최소 거리 찾기
+학교에서 갈 수 있는 경로들, 최소 거리 찾기
+둘다 가능한 목표지점 찾기
+가능한 목표지접 별로 가치를 계산해서 비교하기
+'''
 
 N, M, D, E = map(int, input().split())
 H = list(map(int, input().split()))
