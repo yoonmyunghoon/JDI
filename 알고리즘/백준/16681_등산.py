7222457941c3e3f362c54bb61bd62313df0d9714
yoonmyunghoon <youn1791472@gmail.com>
2021-08-16 20:25:25 +0900

    Solve bj16681

--- 알고리즘/백준/16681_등산.py
+++ 알고리즘/백준/16681_등산.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math, heapq
 sys.stdin = open("16681_등산.txt")
 # input = sys.stdin.readline
 '''
@@ -8,6 +8,27 @@ sys.stdin = open("16681_등산.txt")
 가능한 목표지접 별로 가치를 계산해서 비교하기
 '''
 
+
+def dijkstra(start, graph):
+    dist = [math.inf for _ in range(N+1)]
+    dist[start] = 0
+    heap = []
+    heapq.heapify(heap)
+    heapq.heappush(heap, [dist[start], start])
+    while heap:
+        pre_dist, pre_node = heapq.heappop(heap)
+        if pre_dist > dist[pre_node]:
+            continue
+        for next_node, next_dist in graph[pre_node]:
+            if H[pre_node] >= H[next_node]:
+                continue
+            temp = pre_dist + next_dist
+            if temp < dist[next_node]:
+                dist[next_node] = temp
+                heapq.heappush(heap, [temp, next_node])
+    return dist
+
+
 N, M, D, E = map(int, input().split())
 H = list(map(int, input().split()))
 H.insert(0, 0)
@@ -16,9 +37,17 @@ for i in range(M):
     a, b, n = map(int, input().split())
     G[a].append([b, n])
     G[b].append([a, n])
+dist_from_home = dijkstra(1, G)
+dist_from_school = dijkstra(N, G)
+result = []
+for i in range(N+1):
+    if dist_from_home[i] != math.inf and dist_from_school[i] != math.inf:
+        dist_home_to_school = dist_from_home[i] + dist_from_school[i]
+        result.append(H[i]*E - dist_home_to_school*D)
+if len(result) != 0:
+    print(max(result))
+else:
+    print('Impossible')
 
-print(N, M, D, E)
-print(H)
-for i in G:
-    print(i)
-print()
+
+
