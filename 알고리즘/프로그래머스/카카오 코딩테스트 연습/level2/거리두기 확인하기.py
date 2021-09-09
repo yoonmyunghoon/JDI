d528cb668c997affa8a6c74ef06d39cc84ed6f20
yoonmyunghoon <youn1791472@gmail.com>
2021-09-09 21:41:56 +0900

    Add 거리두기 확인하기

---
+++ 알고리즘/프로그래머스/카카오 코딩테스트 연습/level2/거리두기 확인하기.py
@@ -0,0 +1,50 @@
+def solution(places):
+
+    # def bfs():
+    #
+    #
+    # def check_place():
+
+    answer = []
+    # dx = [-1, 0, 1, 0]
+    # dy = [0, 1, 0, -1]
+    # for place in places:
+    #     flag = 0
+    #     for i in range(5):
+    #         for j in range(5):
+    #             if place[i][j] == 'P':
+    #                 bfs(i, j)
+
+
+
+
+    return answer
+
+
+places_ex = [
+    ["POOOP",
+     "OXXOX",
+     "OPXPX",
+     "OOXOX",
+     "POXXP"],
+    ["POOPX",
+     "OXPXP",
+     "PXXXO",
+     "OXXXO",
+     "OOOPP"],
+    ["PXOPX",
+     "OXOXP",
+     "OXPOX",
+     "OXXOP",
+     "PXPOX"],
+    ["OOOXX",
+     "XOOOX",
+     "OOOXX",
+     "OXOOX",
+     "OOOOO"],
+    ["PXPXP",
+     "XPXPX",
+     "PXPXP",
+     "XPXPX",
+     "PXPXP"]]
+print(solution(places_ex))
